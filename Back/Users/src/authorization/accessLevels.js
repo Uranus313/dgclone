5185464f998ec37500199eebca550147829d4c18
Uranus313 <mehrbodmh82@gmail.com>
2024-08-29 23:50:05 +0330

    role auth

--- Back/Users/src/authorization/accessLevels.js
+++ Back/Users/src/authorization/accessLevels.js
@@ -1,13 +1,13 @@
 export const levels = {commentManage:'commentManage' , orderManage:"orderManage",productManage:"productManage",transactionManage:"transactionManage",userManage:"userManage",sellerManage:"sellerManage",shipmentManage:"shipmentManage",categoryManage:"categoryManage" }
 
 export const accessLevels=[
-    {title: levels.commentManage, name:"مدیریت کامنت"},
-    {title: levels.orderManage, name:"مدیریت سفارش"},
-    {title: levels.productManage, name:"مدیریت محصول"},
-    {title: levels.transactionManage, name:"مدیریت تراکنش"},
-    {title: levels.userManage, name:"مدیریت یوزر"},
-    {title: levels.sellerManage, name:"مدیریت فروشنده"},
-    {title: levels.shipmentManage, name:"مدیریت ارسال"},
-    {title: levels.categoryManage, name:"مدیریت دسته بندی"}
+    {name: levels.commentManage, title:"مدیریت کامنت"},
+    {name: levels.orderManage, title:"مدیریت سفارش"},
+    {name: levels.productManage, title:"مدیریت محصول"},
+    {name: levels.transactionManage, title:"مدیریت تراکنش"},
+    {name: levels.userManage, title:"مدیریت یوزر"},
+    {name: levels.sellerManage, title:"مدیریت فروشنده"},
+    {name: levels.shipmentManage, title:"مدیریت ارسال"},
+    {name: levels.categoryManage, title:"مدیریت دسته بندی"}
 ]
 
--- Back/Users/src/authorization/auth.js
+++ Back/Users/src/authorization/auth.js
@@ -2,95 +2,102 @@ import jwt from "jsonwebtoken";
 import { getUsers } from "../DB/CRUD/user.js";
 import { getSellers } from "../DB/CRUD/seller.js";
 import { getAdmins } from "../DB/CRUD/admin.js";
-import { getTransporters } from "../DB/CRUD/transporter.js";
+import { getEmployees } from "../DB/CRUD/employee.js";
 
-export async function auth(req,res,next,acceptedStatuses){
-    const token = req.cookies["x-auth-token"];
-    if(!token){
-        res.status(401).send({error:"access denied. no token provided."});
-        res.body = {error:"access denied. no token provided."};
-        next();
-        return 
+export async function auth(req, res, next, acceptedStatuses) {
+  const token = req.cookies["x-auth-token"];
+  if (!token) {
+    res.status(401).send({ error: "access denied. no token provided." });
+    res.body = { error: "access denied. no token provided." };
+
+    return;
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWTSECRET);
+    let checker = false;
+    for (let index = 0; index < acceptedStatuses.length; index++) {
+      if (decoded.status == acceptedStatuses[index]) {
+        checker = true;
+      }
     }
-    try {
-        const decoded = jwt.verify(token,process.env.JWTSECRET);
-        let checker = false;
-        for (let index = 0; index < acceptedStatuses.length; index++) {
-            if (decoded.status == acceptedStatuses[index]){
-                checker = true
-            }  
-        }
-        if (!checker){
-            res.status(401).send({error:"access denied. invalid " + acceptedStatuses.join(', ')});
-            res.body = {error:"access denied. invalid " + acceptedStatuses.join(', ')};
-            next();
-            return 
-        }
-        
-        switch (decoded.status) {
-            case "user":
-                const user = (await getUsers(decoded._id)).response;
-                // console.log(user.addresses)
-                if(!user){
-                    res.status(401).send({error:"access denied. invalid user."});
-                    res.body = {error:"access denied. invalid user."};
-                    next();
-                    return 
-                }
-                if (user.isBanned){
-                    res.status(403).send({error:"access denied. you are banned."});
-                    res.body = {error:"access denied. you are banned."};
-                    next();
-                    return 
-                }
-                req.user = user;
-                break;
-            
-            case "seller":
-                const seller = (await getSellers(decoded._id)).response;
-                if(!seller){
-                    res.status(401).send({error:"access denied. invalid seller."});
-                    res.body = {error:"access denied. invalid seller."};
-                    next();
-                    return 
-                }
-                if (seller.isBanned){
-                    res.status(403).send({error:"access denied. you are banned."});
-                    res.body = {error:"access denied. you are banned."};
-                    next();
-                    return 
-                }
-                req.seller = seller;
-                break;  
-            case "admin":
-                const admin = (await getAdmins(decoded._id)).response;
-                if(!admin){
-                    res.status(401).send({error:"access denied. invalid admin."});
-                    res.body = {error:"access denied. invalid admin."};
-                    next();
+    if (!checker) {
+      res
+        .status(401)
+        .send({
+          error: "access denied. invalid " + acceptedStatuses.join(", "),
+        });
+      res.body = {
+        error: "access denied. invalid " + acceptedStatuses.join(", "),
+      };
 
-                    return 
-                }
-                req.admin = admin;
-                break;    
-            case "transporter":
-                const transporter = (await getTransporters(decoded._id)).response;
-                if(!transporter){
-                    res.status(401).send({error:"access denied. invalid transporter."});
-                    res.body = {error:"access denied. invalid transporter."};
-                    next();
-
-                    return 
-                }
-                req.transporter = transporter;
-                break;               
-            default:
-                break;
-        }
-        next();
-    } catch (error) {
-        console.log(error)
-        res.status(400).send({error:"invalid token"});
-        res.body = {error:"invalid token"};
+      return;
     }
+
+    switch (decoded.status) {
+      case "user":
+        const user = (await getUsers(decoded._id)).response;
+        // console.log(user.addresses)
+        if (!user) {
+          res.status(401).send({ error: "access denied. invalid user." });
+          res.body = { error: "access denied. invalid user." };
+
+          return;
+        }
+        if (user.isBanned) {
+          res.status(403).send({ error: "access denied. you are banned." });
+          res.body = { error: "access denied. you are banned." };
+
+          return;
+        }
+        req.user = user;
+        next();
+        break;
+
+      case "seller":
+        const seller = (await getSellers(decoded._id)).response;
+        if (!seller) {
+          res.status(401).send({ error: "access denied. invalid seller." });
+          res.body = { error: "access denied. invalid seller." };
+
+          return;
+        }
+        if (seller.isBanned) {
+          res.status(403).send({ error: "access denied. you are banned." });
+          res.body = { error: "access denied. you are banned." };
+
+          return;
+        }
+        req.seller = seller;
+        next();
+        break;
+      case "admin":
+        const admin = (await getAdmins(decoded._id)).response;
+        if (!admin) {
+          res.status(401).send({ error: "access denied. invalid admin." });
+          res.body = { error: "access denied. invalid admin." };
+
+          return;
+        }
+        req.admin = admin;
+        next();
+        break;
+      case "employee":
+        const employee = (await getEmployees(decoded._id)).response;
+        if (!employee) {
+          res.status(401).send({ error: "access denied. invalid employee." });
+          res.body = { error: "access denied. invalid employee." };
+
+          return;
+        }
+        req.employee = employee;
+        next();
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ error: "invalid token" });
+    res.body = { error: "invalid token" };
+  }
 }
