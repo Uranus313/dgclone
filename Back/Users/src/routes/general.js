cf6ee845e3270bedcd4084b84bba0eaaf306edbe
Uranus313 <mehrbodmh82@gmail.com>
2024-08-29 04:45:29 +0330

    inner check token endpoint

--- Back/Users/src/routes/general.js
+++ Back/Users/src/routes/general.js
@@ -12,6 +12,7 @@ import { getGiftCards } from "../DB/CRUD/giftCard.js";
 import { getTransactions } from "../DB/CRUD/transaction.js";
 import { getTransporters } from "../DB/CRUD/transporter.js";
 import { UserModel } from "../DB/models/user.js";
+import { innerAuth } from "../authorization/innerAuth.js";
 
 
 const router = express.Router();
@@ -421,6 +422,109 @@ router.get("/getUserWallet/:id",(req, res,next) => auth(req, res,next, ["admin"]
     next();
 });
 
+
+router.get("/checkToken/:token",innerAuth,async (req, res, next) => {
+    try {
+        const decoded = jwt.verify(req.params.token,process.env.JWTSECRET);
+        switch (decoded.status) {
+            case "user":
+                const user = (await getUsers(decoded._id)).response;
+                // console.log(user.addresses)
+                if(!user){
+                    res.status(401).send({error:"access denied. invalid user."});
+                    res.body = {error:"access denied. invalid user."};
+                    next()
+                    return 
+                }
+                if (user.isBanned){
+                    res.status(403).send({error:"access denied. you are banned."});
+                    res.body = {error:"access denied. you are banned."};
+                    next();
+                    return 
+                }
+                
+                res.body = {
+                    status : decoded.status,
+                    entity: user
+                };
+                res.send({
+                    status : decoded.status,
+                    entity: user
+                });
+                next();
+
+                break;
+            
+            case "seller":
+                const seller = (await getSellers(decoded._id)).response;
+                if(!seller){
+                    res.status(401).send({error:"access denied. invalid seller."});
+                    res.body = {error:"access denied. invalid seller."};
+                    next();
+                    return 
+                }
+                if (seller.isBanned){
+                    res.status(403).send({error:"access denied. you are banned."});
+                    res.body = {error:"access denied. you are banned."};
+                    next();
+                    return 
+                }
+                res.body = {
+                    status : decoded.status,
+                    entity: seller
+                };
+                res.send({
+                    status : decoded.status,
+                    entity: seller
+                });
+                next();
+                break;  
+            case "admin":
+                const admin = (await getAdmins(decoded._id)).response;
+                if(!admin){
+                    res.status(401).send({error:"access denied. invalid admin."});
+                    res.body = {error:"access denied. invalid admin."};
+                    next();
+                    return 
+                }
+                res.body = {
+                    status : decoded.status,
+                    entity: admin
+                };
+                res.send({
+                    status : decoded.status,
+                    entity: admin
+                });
+                next();
+                break;    
+            case "transporter":
+                const transporter = (await getTransporters(decoded._id)).response;
+                if(!transporter){
+                    res.status(401).send({error:"access denied. invalid transporter."});
+                    res.body = {error:"access denied. invalid transporter."};
+                    next();
+                    return 
+                }
+                res.body = {
+                    status : decoded.status,
+                    entity: transporter
+                };
+                res.send({
+                    status : decoded.status,
+                    entity: transporter
+                });
+                next();
+                break;               
+            default:
+                break;
+        }
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(400).send({error:"invalid token"});
+        res.body = {error:"invalid token"};
+    }
+})
 router.get("/getSellerWallet/:id",(req, res,next) => auth(req, res,next, ["admin"]) , async (req, res, next) =>{
     const {error} = validateId(req.params.id);
     if (error){
