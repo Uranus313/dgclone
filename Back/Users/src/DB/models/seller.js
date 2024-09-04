819757a9c16a95e9f0308e4099085569f5dea308
Uranus313 <mehrbodmh82@gmail.com>
2024-09-04 13:44:42 +0330

    inner get seller card

--- Back/Users/src/DB/models/seller.js
+++ Back/Users/src/DB/models/seller.js
@@ -17,6 +17,7 @@ const sellerSchema  = new mongoose.Schema(
             }},  
         isCompelete: Boolean,
         isVerified: Boolean,
+        rating: {type: Number , required: true , default: 0},
         phoneNumber: {type: String , required : true},
         entityType: {type: String,enum: ["individual" , "legal"], required: true,
             validate: {
---
+++ Back/Users/src/routes/inner.js
@@ -0,0 +1,55 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { innerAuth } from "../authorization/innerAuth";
+import { getSellers } from "../DB/CRUD/seller";
+
+
+const router = express.Router();
+
+router.get("sellerCard/:id",innerAuth,async (req, res, next) => {
+    const {error} = validateId(req.params.id);
+    if (error){
+        res.status(400).send({error : error.details[0].message});
+            res.body = {error : error.details[0].message};
+        next();
+        return;
+    } 
+    try {
+        const result = await getSellers(req.params.id);
+        if (result.error){
+            res.status(400).send({error : result.error});
+            res.body = {error : result.error};
+            next();
+            return;
+        }
+        const seller = result.response;
+        if(!seller){
+            res.status(404).send({error : "seller not found"});
+            res.body = {error : "seller not found"};
+            next();
+            return;
+        }
+        if(seller.isBanned){
+            res.status(403).send({error : "seller is banned"});
+            res.body = {error : "seller is banned"};
+            next();
+            return;
+        }if(!seller.isVerified){
+            res.status(403).send({error : "seller is not verified"});
+            res.body = {error : "seller is not verified"};
+            next();
+            return;
+        }
+
+        const sellerCard = {
+            title : seller.storeInfo.commercialName,
+            rating : seller.rating
+        }
+        res.body = sellerCard;
+        res.send(sellerCard)
+    } catch (err) {
+        console.log("Error",err);
+        res.body = {error:"internal server error"};
+        res.status(500).send({error:"internal server error"});
+    }
+})
