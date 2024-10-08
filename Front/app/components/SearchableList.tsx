b554f1675373a880a4e0dabedf0edce419c5f0a1
the-mr-poarbear <arsam.amr@gmail.com>
2024-10-08 15:05:01 +0330

    infinite scroll done

--- Front/app/components/SearchableList.tsx
+++ Front/app/components/SearchableList.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useRef, useState } from 'react';
-import { Color } from './Interfaces/interfaces';
+
 
 
 interface Props{
---
+++ Front/app/hooks/useGetProducts.tsx
@@ -0,0 +1,39 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Brand, ProductInterface, SellerAddProdctCard } from "../components/Interfaces/interfaces";
+import { useRef } from "react";
+
+interface Props {
+    limit: number,
+    categoryID: string
+}
+
+function useGetProducts({ limit, categoryID }: Props) {
+    return useInfiniteQuery({
+        queryKey: ['product', categoryID],
+        initialPageParam: 1,
+        queryFn: async ({ pageParam = 1 }) => {
+            const result = await fetch(`http://localhost:8080/products/product/?limit=${limit}&offset=${pageParam * limit}&&CateID=${categoryID}`, {
+                credentials: 'include'
+            });
+
+            const jsonResult = await result.json();
+
+            if (result.ok) {
+                return {
+                    products: jsonResult.products as SellerAddProdctCard[],
+                    hasMore: jsonResult.hasMore as boolean
+                };
+            } else {
+                throw new Error(jsonResult.error);
+            }
+        },
+        staleTime: 6 * 60 * 60 * 1000,
+        refetchOnWindowFocus: false,
+        retry: 2,
+        getNextPageParam: (lastPage, allPages) => {
+            return lastPage.hasMore ? allPages.length + 1 : undefined;
+        }
+    });
+}
+
+export default useGetProducts;
