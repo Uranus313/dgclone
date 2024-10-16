88f63a5a521eb2faf99141109ac1c808de922203
AmirGhz17 <amirreza.ghazizade@gmail.com>
2024-10-16 17:04:25 +0330

    amir comment

--- Front/app/(Customer)/Footer.tsx
+++ Front/app/(Customer)/Footer.tsx
@@ -12,6 +12,7 @@ const Footer = () => {
                 <a className="link link-hover">Marketing</a>
                 <a className="link link-hover">Advertisement</a>
             </nav>
+            
             <nav>
                 <h6 className="footer-title">Company</h6>
                 <a className="link link-hover">About us</a>
